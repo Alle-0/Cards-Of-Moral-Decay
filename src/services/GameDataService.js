ee3449be2452efd2083e4591a61fc8d81bd68718
Alle-0 <alle0.github@example.com>
2026-02-02 21:03:05 +0100

    Release 3.7.1: Hotfix for Notifications & Deploy Update

--- src/services/GameDataService.js
+++ src/services/GameDataService.js
@@ -23,7 +23,7 @@ class GameDataService {
         this.darkPack = { nere: [], bianche: [] };
 
         this.isLoaded = false;
-        this.minVersion = "3.7.0";
+        this.minVersion = "3.7.1";
         this.downloadUrl = null;
     }
 
